de5373e94dfc7f3cb82ab96b31899fe5d7040e02
0tr1cala-v <marenkov2015@mail.ru>
2024-10-25 12:57:35 +0300

    module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-api = "7766619527:AAHdVn-vBNCerMJKr6ysvaLBofghDdhTMbg"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
